b365619950a1298944d5feedc2dc29d238c1705e
Szymon Hankus <dephan@protonmail.com>
2023-03-24 00:07:49 +0100

    initial commit

---
+++ Pracownicy/JobTitles.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pracownicy
+{
+    enum JobTitles
+    {
+        DESIGNER, PROGRAMMMER, MANAGER
+    }
+
+    static class JobTitlesMethods
+    {
+        public static String getString(JobTitles title)
+        {
+            switch (title)
+            {
+                case JobTitles.DESIGNER:
+                    return "Projektant";
+                case JobTitles.PROGRAMMMER:
+                    return "Programista";
+                case JobTitles.MANAGER:
+                    return "Menedzer";
+            }
+            return "unknown";
+        } 
+    }
+}
---
+++ Pracownicy/Presenter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pracownicy
+{
+    class Presenter
+    {
+        private Form1 _view;
+        private Model _model;
+
+        public Presenter(Form1 view, Model model)
+        {
+            this._view = view;
+            this._model = model;
+        }
+
+        public void OnClickAdd()
+        {
+
+        }
+    }
+}
